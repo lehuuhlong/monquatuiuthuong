ef9581810590dd4d58aeb2d9cfedd1ce1294c7bf
Hoang Long <63138417+lehuuhlong@users.noreply.github.com>
2026-07-05 00:36:01 +0700

    Update CONFIG for personalized messages

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Dỗi à!',
+    introTitle: 'Vân à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao dỗi cứ đi nhầm đường
+    Mà sao Vân cứ đi nhầm đường
     Lạc vào tim Long lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'haha',
-    title: 'Phải chăng dỗi đã yêu long ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng dỗi đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Vân đã yêu Long ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Vân đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Long <33',
     btnNo: 'Không, Long à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Long <3',
     btnReply: 'Gửi cho Long <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Long biết mà 🥰. Yêu dỗi nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h vô chơi game cùng Long nhé công chúa.',
+    messDesc: 'Ngày mai cùng Long đi chơi nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://m.me/longlee2000'
 }
